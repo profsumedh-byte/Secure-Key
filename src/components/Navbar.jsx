a1c8c95f6966914bc800426d801cd83b5c379b0d
profsumedh-byte <prof.sumedh@gmail.com>
2026-06-11 18:06:36 +0530

    github link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className='flex justify-between h-14 px-60 py-3.5 bg-linear-to-r from-indigo-400 to-cyan-400 text-white font-bold text-2xl'>
      
       <h1><span>&lt;</span> SecureKey <span>/</span> <span>&gt;</span></h1>
-      <a href="https://github.com/"><div className="flex justify-between gap-2.5 items-center" ><span>GitHub</span><span><img className="h-7 w-7" src={github} alt="github_logo" /></span></div></a>
+      <a href="https://secure-key-green.vercel.app/"><div className="flex justify-between gap-2.5 items-center" ><span>GitHub</span><span><img className="h-7 w-7" src={github} alt="github_logo" /></span></div></a>
     </div>
   )
 }
